b5d0dc8cd9e84d158333aecd5bc146bddd6a1813
EarthSalamander <jeizyy30@hotmail.fr>
2020-04-03 14:41:41 +0200

    new working base

--- DotA Classic Mod/game/dota_addons/DotA_Classic/scripts/vscripts/lua_abilities/axe_counter_helix_lua/axe_counter_helix_lua.lua
+++
@@ -1,8 +0,0 @@
-axe_counter_helix_lua = class({})
-LinkLuaModifier( "modifier_axe_counter_helix_lua", "lua_abilities/axe_counter_helix_lua/modifier_axe_counter_helix_lua", LUA_MODIFIER_MOTION_NONE )
-
---------------------------------------------------------------------------------
--- Passive Modifier
-function axe_counter_helix_lua:GetIntrinsicModifierName()
-	return "modifier_axe_counter_helix_lua"
-end
