c0b9b2943c9f94a3c70bfde381757442ff0a03fd
EarthSalamander <jeizyy30@hotmail.fr>
2021-04-06 22:57:42 +0200

    Added deniable ward modifier if planted in neutral camp

---
+++ DotA Classic Mod/game/dota_addons/DotA_Classic/scripts/vscripts/components/neutral_creeps/modifiers/modifier_imba_ward_deniable.lua
@@ -0,0 +1,9 @@
+modifier_imba_ward_deniable = modifier_imba_ward_deniable or class({})
+
+function modifier_imba_ward_deniable:IsHidden() return true end
+function modifier_imba_ward_deniable:IsPurgable() return false end
+function modifier_imba_ward_deniable:GetEffectName() return "particles/msg_fx/msg_deniable.vpcf" end
+function modifier_imba_ward_deniable:GetEffectAttachType() return PATTACH_OVERHEAD_FOLLOW end
+function modifier_imba_ward_deniable:CheckState() return {
+	[MODIFIER_STATE_SPECIALLY_DENIABLE] = true,
+} end
